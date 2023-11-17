8f644d79de867f4ae8e52827d2394b62ccb2687a
abaduna <64230830+abaduna@users.noreply.github.com>
2023-11-17 11:59:28 -0300

    variables

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 import './App.css';
 
 function App() {
-  const [time, setTime] = useState(0);
-  const [borderWidth, setBorderWidth] = useState(1);
+  const [timeSecund, SetTimeSecund] = useState(0);
+  const [timeMinut, SetTimeMinut]  = useState(0);
   const intervalRef = useRef(null);
+  const [handledStartStop, SetHandledStartStop] = useState(false);
 
-  const handleStart = () => {
-    setBorderWidth(1);
+  const handleToggle = () => {
+    if (handledStartStop) {
+      handleStop();
+    } else {
+      handleStart();
+    }
+    SetHandledStartStop(!handledStartStop);
+  };
+
+
+  const handleStart = useCallback(() => {
+    console.log(`handleStart`);
 
     intervalRef.current = setInterval(() => {
-      setBorderWidth((prevWidth) => (prevWidth >= 60 ? 1 : prevWidth + 1)); // Ajuste de la lógica
-
-      setTime((prevTime) => prevTime + 1);
-    }, 1000);
-  };
+		console.log(`SetTimeSecund ${timeSecund}`);
+		SetTimeSecund((prevWidth) => (prevWidth === 60 ? 0 : prevWidth + 1));
+	  }, 1000);
+	  setInterval(() => {
+		console.log(`SetTimeMinut ${timeMinut}`);
+		SetTimeMinut((prevWidth2) => (prevWidth2 === 60 ? 0 : prevWidth2 + 1));
+	  }, 60000);
+  });
 
   const handleStop = () => {
+    console.log(`handleStop`);
     clearInterval(intervalRef.current);
   };
-
   useEffect(() => {
     return () => {
       clearInterval(intervalRef.current);
@@ -28,33 +42,35 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1 className='cronometro'>Cronómetro en react</h1>
-      <span className='cronometro'>Cronómetro: {time}</span>
-
-      <div className="porcentaje" style={{ '--porcentaje': borderWidth, '--color': 'yellow' }}>
+<div className="App">
+	<h1 className='titulo'>Cronómetro en react</h1>
+	
+    <div className="container">
+      <div className="segundos" style={{ '--porcentaje': `${timeSecund * 1.667}`,  '--color': 'yellow' }}>
         <svg width="150" height="150">
+          <circle r="70" cx="50%" cy="50%" pathLength="100"></circle>
           <circle r="70" cx="50%" cy="50%" pathLength="100"></circle>
         </svg>
       </div>
 
-      <div className="porcentaje" style={{ '--porcentaje': borderWidth, '--color': 'rgb(60, 255, 0)' }}>
+      <div className="minutos"  style={{  '--porcentaje': `${timeMinut * 1.667}%`,  '--color': 'rgb(60, 255, 0)' }}>
         <svg width="180" height="180">
+          <circle r="80" cx="50%" cy="50%" pathLength="100"></circle>
           <circle r="80" cx="50%" cy="50%" pathLength="100"></circle>
         </svg>
       </div>
+	    <span className='cronometro'>{timeMinut}:{timeSecund}</span>  
 
-      <div className="porcentaje" style={{ '--porcentaje': borderWidth, '--color': 'rgb(0, 255, 157)' }}>
-        <svg width="200" height="200">
-          <circle r="90" cx="50%" cy="50%" pathLength="100"></circle>
-        </svg>
-      </div>
 
-      <div>
-        <button onClick={handleStart}>Empezar</button>
-        <button onClick={handleStop}>Parar</button>
-      </div>
-    </div>
+
+      <div className='btn-centar'>
+      	<button onClick={handleToggle}>{handledStartStop ? 'Parar' : 'Empezar'}</button>
+      </div>	
+
+
+	</div>
+
+</div>
   );
 }
 
